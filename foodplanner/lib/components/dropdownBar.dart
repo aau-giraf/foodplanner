5cda0cd8ae8379b9dcb1f76dd059c8e3c505ac02
Severin1028 <fsla22@student.aau.dk>
2024-10-09 14:56:42 +0200

    fix dropdowmbar

---
+++ foodplanner/lib/components/dropdownBar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:foodplanner/config/colors.dart';
+import 'package:foodplanner/config/text_styles.dart';
+
+class DropdownBar extends StatefulWidget {
+  final double width;
+  final String? selectedValue;
+  final List<String> items;
+  final ValueChanged<String?> onChanged;
+  final Color color;
+
+  DropdownBar({
+    required this.width,
+    required this.items,
+    this.selectedValue,
+    required this.onChanged,
+    this.color = AppColors.textFieldBackground // default color
+  });
+
+  @override
+  _DropdownBarState createState() => _DropdownBarState();
+}
+
+class _DropdownBarState extends State<DropdownBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: widget.width,
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 255, 255, 255),
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(color: const Color.fromARGB(255, 0, 0, 0)), 
+      ),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<String>(
+          value: widget.selectedValue,
+          hint: Text(
+            'Vælg din role',
+          ),
+          icon: Icon(Icons.arrow_drop_down, color: const Color.fromARGB(255, 0, 0, 0)),
+          isExpanded: true,
+          items: widget.items.map((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(
+                value,
+              ),
+            );
+          }).toList(),
+          onChanged: widget.onChanged,
+        ),
+      ),
+    );
+  }
+}
