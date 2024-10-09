0fe4e3aece60791fbdf6bb97350b4d195b2cadb6
Andreas Jack Christiansen <andreas.jack.2002@gmail.com>
2024-10-09 13:22:53 +0200

    added login api call and updaed user to support new role

--- foodplanner/lib/pages/login_page.dart
+++ foodplanner/lib/pages/login_page.dart
@@ -1,20 +1,80 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_sficon/flutter_sficon.dart';
 import 'package:foodplanner/components/button.dart';
 import 'package:foodplanner/components/text_field.dart';
+import 'package:foodplanner/components/user.dart';
 import 'package:foodplanner/config/colors.dart';
 import 'package:foodplanner/config/text_styles.dart';
 import 'forgot_password_page.dart';
 import 'signup_page.dart';
 
-class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
   // Text editing controllers
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
-  void signUserIn(BuildContext context) {}
+  // Text error messages
+  String emailError = '';
+  String passwordError = '';
+
+  void updateErrorState(String field, String error) {
+    setState(() {
+      switch (field) {
+        case 'Email':
+          emailError = error;
+          break;
+        case 'Password':
+          passwordError = error;
+          break;
+      }
+    });
+  }
+
+  void handleErrors(Map<String, dynamic> error) {
+    updateErrorState('Email', error['Email'] != null ? error['Email'][0] : '');
+    updateErrorState(
+        'Password', error['Password'] != null ? error['Password'][0] : '');
+  }
+
+  void signUserIn(BuildContext context) async {
+    try {
+      final response =
+          await loginUser(usernameController.text, passwordController.text);
+
+      if (response.statusCode == 200) {
+        var body = jsonDecode(response.body);
+        User user = User.fromJson(body);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+                'Du er nu logget ind. ${user.firstName}, ${user.lastName}'),
+            backgroundColor: Colors.green,
+            duration: Duration(seconds: 5),
+          ),
+        );
+      } else {
+        var error = jsonDecode(response.body);
+        handleErrors(error);
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Der opstod et problem ved login: $e'),
+          backgroundColor: Colors.red,
+          duration: Duration(seconds: 5),
+        ),
+      );
+    }
+  }
 
   void loginInpage() {}
 
@@ -50,13 +110,13 @@ class LoginPage extends StatelessWidget {
             CustomTextField(
                 hintText: "Brugernavn",
                 controller: usernameController,
-                errorText: ''),
+                errorText: emailError),
             const SizedBox(height: 15),
             CustomTextField(
                 hintText: "Adgangskode",
                 obscureText: true,
                 controller: passwordController,
-                errorText: ''),
+                errorText: passwordError),
             const SizedBox(height: 10),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 150),
