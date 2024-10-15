b3aff24af90356878e3e2f346f531e329e2352c9
koki6769 <113096274+koki6769@users.noreply.github.com>
2024-10-15 11:44:41 +0200

    added config

---
+++ foodplanner/lib/config/text_styles.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:foodplanner/config/colors.dart';
+
+class AppTextStyles {
+  static const TextStyle headline1 = TextStyle(
+    fontSize: 32.0,
+    fontWeight: FontWeight.bold,
+    color: AppColors.textPrimary, // You can use AppColors here if needed
+  );
+
+  static const TextStyle standard = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.normal,
+    color: AppColors.textPrimary,
+  );
+
+  static const TextStyle buttonText = TextStyle(
+    fontSize: 26.0,
+    fontWeight: FontWeight.bold,
+    color: AppColors.textSecondary,
+  );
+
+  static const TextStyle errorText = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.normal,
+    color: AppColors.errorText,
+  );
+}
