8195dbabe9339f3ebc3e691473b4c13875d415bf
Andreas Jack Christiansen <andreas.jack.2002@gmail.com>
2024-10-16 13:32:32 +0200

    cleanup

--- foodplanner/lib/main.dart
+++ foodplanner/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodplanner/components/nav_bar.dart';
 import 'package:foodplanner/pages/login_page.dart';
-import 'package:foodplanner/pages/login_start.dart';
 import 'package:foodplanner/pages/signup_page.dart';
 import 'package:foodplanner/pages/admin_approve_page.dart';
 
