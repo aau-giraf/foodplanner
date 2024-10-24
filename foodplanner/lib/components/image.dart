c92f4d6cff8e9976fc809229242a6a9ba1b31c09
alexrefshauge <nk02hb@aau.student.dk>
2024-10-24 11:24:40 +0200

    use imagesApi

--- foodplanner/lib/components/image.dart
+++ foodplanner/lib/components/image.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
+import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
+import 'package:foodplanner/api/generator/openapi_config.dart';
+import 'package:foodplanner/api/openapi/lib/api.dart';
 import 'package:http/http.dart' as http;
+import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 class FoodImage {
-
   final int foodImageId;
 
   FoodImage({required this.foodImageId});
@@ -15,10 +18,14 @@ class FoodImage {
   }
 
   Future<FoodImage> fetchImage() async {
-    final response = await http.get(Uri.parse('http://127.0.0.1:80/api/Images/GetPresignedImageLink'));
+    ImagesApi().apiImagesGetPresignedImageLinkGet(foodImageId: 1);
+
+    final response = await http
+        .get(Uri.parse('http://127.0.0.1:80/api/Images/GetPresignedImageLink'));
 
     if (response.statusCode == 200) {
-      return FoodImage.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+      return FoodImage.fromJson(
+          jsonDecode(response.body) as Map<String, dynamic>);
     } else {
       throw Exception('Kunne ikke hente billede');
     }
