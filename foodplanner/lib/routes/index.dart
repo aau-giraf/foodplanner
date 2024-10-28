8f11f0ed92b8e57ea29c42128a9f9facbd54c601
Rune <rforsb22@student.aau.dk>
2024-10-28 10:13:09 +0100

    Added routing for children_madpakke and children_se_madpakke. Updated pubspec.lock and .yaml.

--- foodplanner/lib/routes/index.dart
+++ foodplanner/lib/routes/index.dart
@@ -5,6 +5,9 @@ import 'package:foodplanner/pages/forgot_password_page.dart';
 import 'package:foodplanner/pages/parent_page.dart';
 import 'package:foodplanner/pages/signup_page.dart';
 import 'package:foodplanner/pages/student_page.dart';
+import 'package:foodplanner/pages/landing_page_children_madpakke.dart';
+import 'package:foodplanner/pages/landing_page_children_se_madpakke.dart';
+import 'package:foodplanner/pages/student_page.dart';
 import 'package:foodplanner/pages/teacher_page.dart';
 import 'package:foodplanner/routes/paths.dart';
 import 'package:foodplanner/routes/user_roles.dart';
@@ -12,7 +15,7 @@ import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import '../pages/login_page.dart'; 
 import '../pages/unauthorized_page.dart';
-import '../pages/home_page.dart'; // Import HomePage
+import '../pages/home_page.dart';
 
 final router = GoRouter(
   routes: [
@@ -35,6 +38,11 @@ final router = GoRouter(
       builder: (context, state) => ForgotPasswordPage(),
     ),
 
+    GoRoute(
+      path: '/children_se_madpakke',
+      builder: (context, state) => ChildLandingPageMadpakke(),
+    ),
+
 
     //no need for wildcard handling as flutter already does it 
    
@@ -50,7 +58,7 @@ final router = GoRouter(
       path: STUDENT_ROOT,
       builder: (context, state) {
         final authProvider = Provider.of<AuthProvider>(context, listen: false);
-        return authProvider.hasRole(ROLES.student) ? const StudentPage() : const UnauthorizedPage();
+        return authProvider.hasRole(ROLES.student) ? const ChildLandingPageMadpakke() : const UnauthorizedPage();
       },
     ),
     GoRoute(
