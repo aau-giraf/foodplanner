789a7c80887b7ec90879881eb56a7f6a71bbbdd0
Tobias Bay <tbay22@student.aau.dk>
2024-10-28 19:49:37 +0100

    modified fetchImage, so it wont call api twice

--- foodplanner/lib/components/image.dart
+++ foodplanner/lib/components/image.dart
@@ -1,16 +1,12 @@
 import 'dart:convert';
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-import 'package:foodplanner/api/generator/openapi_config.dart';
+
 import 'package:foodplanner/api/openapi/lib/api.dart';
-import 'package:http/http.dart' as http;
-import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 class FoodImage {
   final int foodImageId;
 
   FoodImage({required this.foodImageId});
 
-  // A method to create an instance from JSON
   factory FoodImage.fromJson(Map<String, dynamic> json) {
     return FoodImage(
       foodImageId: json['foodImageId'],
@@ -18,10 +14,9 @@ class FoodImage {
   }
 
   Future<FoodImage> fetchImage() async {
-    ImagesApi().apiImagesGetPresignedImageLinkGet(foodImageId: 1);
-
-    final response = await http
-        .get(Uri.parse('http://127.0.0.1:80/api/Images/GetPresignedImageLink'));
+    final response = await ImagesApi()
+        .apiImagesGetPresignedImageLinkGetWithHttpInfo(
+            foodImageId: foodImageId);
 
     if (response.statusCode == 200) {
       return FoodImage.fromJson(
