76ffdc7ca78a146965a822191f74e1a805775d14
Andreas Jack Christiansen <andreas.jack.2002@gmail.com>
2024-11-04 15:05:20 +0100

    created a generic settings component

---
+++ foodplanner/lib/components/settings_widget.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sficon/flutter_sficon.dart';
+import 'package:foodplanner/config/colors.dart';
+import 'package:foodplanner/config/text_styles.dart';
+
+class SettingsWidget extends StatefulWidget {
+  final IconData leftIcon;
+  final String title;
+  final dynamic cta;
+  const SettingsWidget({
+    super.key,
+    required this.leftIcon,
+    required this.title,
+    required this.cta,
+  });
+
+  @override
+  State<SettingsWidget> createState() => _SettingsWidgetState();
+}
+
+class _SettingsWidgetState extends State<SettingsWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Card(
+        child: Row(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0), // Add rounded corners
+                child: Container(
+                  color: AppColors.primary,
+                  width: 50,
+                  height: 50,
+                  child: Center(
+                    child: SFIcon(
+                      widget.leftIcon,
+                      fontSize: 24,
+                      color: AppColors.textSecondary,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Text(
+                widget.title,
+                style: AppTextStyles.bigText,
+                softWrap: true, // Allow text to wrap
+              ),
+            ),
+            widget.cta,
+            SizedBox(width: 10),
+            /* IconButton(
+              icon: SFIcon(SFIcons.sf_chevron_forward),
+              onPressed: () {},
+            ), */
+          ],
+        ),
+      ),
+    );
+  }
+}
--- foodplanner/lib/pages/settings.dart
+++ foodplanner/lib/pages/settings.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sficon/flutter_sficon.dart';
+import 'package:foodplanner/components/settings_widget.dart';
+import 'package:foodplanner/config/colors.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
+
+  Widget ctaButtons() {
+    return Row(
+      children: [
+        IconButton(
+          padding: EdgeInsets.zero,
+          icon: SFIcon(
+            SFIcons.sf_checkmark_square_fill,
+            color: AppColors.primary,
+            fontSize: 36,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          padding: EdgeInsets.zero,
+          icon: SFIcon(
+            SFIcons.sf_xmark_square_fill,
+            color: AppColors.errorText,
+            fontSize: 36,
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +37,16 @@ class Settings extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Indstillinger'),
       ),
-      body: const Center(
-        child: Text('Indstillinger'),
+      body: Column(
+        children: [
+          Center(
+            child: SettingsWidget(
+              leftIcon: SFIcons.sf_graduationcap_fill,
+              title: 'John Hansen',
+              cta: ctaButtons(),
+            ),
+          ),
+        ],
       ),
     );
   }
