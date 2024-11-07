f6d850ded6c507fe64aeeaee5c6e04327b47e2d8
Andreas Jack Christiansen <andreas.jack.2002@gmail.com>
2024-11-07 17:15:46 +0100

    started working on schoolclass settings

--- foodplanner/lib/components/nav_bar.dart
+++ foodplanner/lib/components/nav_bar.dart
@@ -4,7 +4,7 @@ import 'package:foodplanner/config/colors.dart';
 import 'package:foodplanner/pages/additional_lunch_box.dart';
 import 'package:foodplanner/pages/landing_page.dart';
 import 'package:foodplanner/pages/profile.dart';
-import 'package:foodplanner/pages/settings.dart';
+import 'package:foodplanner/pages/settings/settings.dart';
 
 class NavBar extends StatefulWidget {
   const NavBar({super.key});
