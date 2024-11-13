cdb956152bcefb4cff5402bf5fc1a6e92546ea38
Jacob Reeslev <jreesl22@student.aau.dk>
2024-11-13 15:18:55 +0100

    bug fixed

--- foodplanner/lib/api/generator/openapi_config.dart
+++ foodplanner/lib/api/generator/openapi_config.dart
@@ -1,4 +1,4 @@
-// Openapi Generator last run: : 2024-11-13T13:30:39.679879
+// Openapi Generator last run: : 2024-11-13T14:19:16.568680
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 @Openapi(
