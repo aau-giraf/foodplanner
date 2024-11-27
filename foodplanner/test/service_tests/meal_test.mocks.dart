73efa0f9af0d7009b5866a1cc2b8fd35651dbcb0
Tobias Bay <tbay22@student.aau.dk>
2024-11-27 12:31:16 +0100

    renamed:    foodplanner/lib/pages/profilePage.dart -> foodplanner/lib/pages/profile_page.dart 	renamed:    foodplanner/lib/pages/settingsPage.dart -> foodplanner/lib/pages/settings_page.dart

--- foodplanner/test/service_tests/meal_test.mocks.dart
+++ foodplanner/test/service_tests/meal_test.mocks.dart
@@ -48,8 +48,8 @@ class _FakeStreamedResponse_1 extends _i1.SmartFake
 /// A class which mocks [Client].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MyMockClient extends _i1.Mock implements _i2.Client {
-  MyMockClient() {
+class MockClient extends _i1.Mock implements _i2.Client {
+  MockClient() {
     _i1.throwOnMissingStub(this);
   }
 
