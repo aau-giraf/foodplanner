e2b6b185ecd3e2a9e4e9528b7d8360e096ca264a
Dandan Zhao <dandanzhao08@gmail.com>
2025-09-26 11:37:09 +0200

    Advarsel ved forkert login

--- foodplanner/lib/pages/login_page.dart
+++ foodplanner/lib/pages/login_page.dart
@@ -3,6 +3,7 @@ import 'package:foodplanner/components/button.dart';
 import 'package:foodplanner/components/text_field.dart';
 import 'package:foodplanner/config/colors.dart';
 import 'package:foodplanner/config/text_styles.dart';
+import 'package:foodplanner/models/user.dart';
 import 'package:foodplanner/routes/paths.dart';
 import 'package:foodplanner/services/api_config.dart';
 import 'package:foodplanner/pages/forgot_password_page.dart';
@@ -54,6 +55,30 @@ class LoginPageState extends State<LoginPage> {
   }
 
   void signUserIn(BuildContext context) async {
+    if(usernameController.text.isEmpty && passwordController.text.isEmpty){
+      setState(() {
+        emailError = 'Email mangler';
+        passwordError = 'Adgangskode mangler';
+      });
+
+    } else if (usernameController.text.isEmpty){
+      setState(() {
+        emailError  = 'Email mangler';
+        passwordError = '';
+      });
+
+    }else if(passwordController.text.isEmpty){
+      setState(() {
+        emailError = '';
+        passwordError = 'Adgangskode mangler';
+      });
+    } else {
+      setState(() {
+        emailError = '';
+        passwordError = '';
+      });
+    }
+
     try {
       final role = await LoginPage.authService
           .fetchAuthData(usernameController.text, passwordController.text);
@@ -84,7 +109,7 @@ class LoginPageState extends State<LoginPage> {
         SnackBar(
           content: Text('Der opstod et problem ved login: ${e.message}'),
           backgroundColor: Colors.red,
-          duration: Duration(seconds: 5),
+          duration: Duration(seconds: 7),
         ),
       );
     } else {
@@ -92,9 +117,10 @@ class LoginPageState extends State<LoginPage> {
         SnackBar(
           content: Text('Der opstod et ukendt problem ved login: $e'),
           backgroundColor: Colors.red,
-          duration: Duration(seconds: 5),
+          duration: Duration(seconds: 7),
         ),
       );
+      
     }
   }
 }
