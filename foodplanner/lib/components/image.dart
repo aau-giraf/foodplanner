d610c48d4f5540cb1b829a51f200c337fc6de838
Kresten Laust <krestenlaust@gmail.com>
2025-10-31 10:09:24 +0100

    Const initialization of immutables

--- foodplanner/lib/components/image.dart
+++ foodplanner/lib/components/image.dart
@@ -10,7 +10,7 @@ class FoodImage extends StatelessWidget {
   final double borderRadius;
   final imageUrl = 'https://cdn-icons-png.flaticon.com/512/739/739249.png';
 
-  FoodImage({
+  const FoodImage({
     required this.foodImageId,
     this.width = 250,
     this.height = 250,
