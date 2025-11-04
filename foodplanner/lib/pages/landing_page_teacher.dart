3247b20121fa7f3151f31c7965661754131976d7
Peter Slessing <peter@slessing.dk>
2025-11-04 10:25:47 +0100

    fixed import name

--- foodplanner/lib/pages/landing_page_teacher.dart
+++ foodplanner/lib/pages/landing_page_teacher.dart
@@ -10,7 +10,7 @@ import 'package:foodplanner/services/api_config.dart';
 import 'package:foodplanner/components/nav_bar.dart';
 import 'package:foodplanner/config/colors.dart';
 import 'package:foodplanner/config/text_styles.dart';
-import 'package:foodplanner/components/Custom_List_Item.dart';
+import 'package:foodplanner/components/custom_list_item.dart';
 import 'package:foodplanner/components/button.dart';
 import 'package:foodplanner/models/user.dart' as model;
 import 'package:foodplanner/services/user_service.dart';
