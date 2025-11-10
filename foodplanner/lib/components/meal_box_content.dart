147b67f13166fbe14c10802741d4862fbef46e2c
Buskemor <130304167+Buskemor@users.noreply.github.com>
2025-11-10 23:26:02 +0100

    Calendar navigation arrows added

--- foodplanner/lib/components/meal_box_content.dart
+++ foodplanner/lib/components/meal_box_content.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sficon/flutter_sficon.dart';
 import 'package:foodplanner/components/button.dart';
 import 'package:foodplanner/components/image.dart';
 import 'package:foodplanner/pages/landing_page_children_se_madpakke.dart';
@@ -7,33 +8,18 @@ import 'package:provider/provider.dart';
 
 class Mealboxcontent extends StatelessWidget {
   const Mealboxcontent({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return Consumer<MealNotifier>(
       builder: (context, mealNotifier, child) {
         if (mealNotifier.meal == null) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              // sabrina carpenter tho :flushedEmoj:
-              const Text(
-                'Ingen madpakke at vise',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ],
-          );
+          return _buildNavigationArrows(mealNotifier);
         }
         return Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Text(
-                mealNotifier.meal!.name,
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-            ),
+            _buildNavigationArrows(mealNotifier), 
+            SizedBox(height: 20),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 40),
               child: ClipRRect(
@@ -49,7 +35,8 @@ class Mealboxcontent extends StatelessWidget {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => ChildLandingPageSeMadpakke()),
+                      builder: (context) => ChildLandingPageSeMadpakke()
+                    ),
                   );
                 },
                 text: 'Se madpakke',
@@ -59,6 +46,41 @@ class Mealboxcontent extends StatelessWidget {
           ],
         );
       },
+    );
+  }
+
+  /// This widget also includes the text between the arrows.
+  Widget _buildNavigationArrows(MealNotifier mealNotifier) {
+    final EdgeInsets horizontalInsets = EdgeInsets.symmetric(horizontal: 10);
+    final double buttonHeight = 40;
+    final double buttonWidth = 80;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Padding(
+          padding: horizontalInsets,
+          child: CustomButton(
+            onTab: () => mealNotifier.updateDate(mealNotifier.selectedDate.subtract(Duration(days: 1))), 
+            customHeight: buttonHeight, 
+            customWidth: buttonWidth, 
+            icon: SFIcon(SFIcons.sf_arrow_backward)
+          ),
+        ),
+        Text(
+          mealNotifier.meal != null ? mealNotifier.meal!.name : 'Ingen madpakke at vise',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Padding(
+          padding: horizontalInsets, 
+          child: CustomButton(
+            onTab: () => mealNotifier.updateDate(mealNotifier.selectedDate.add(Duration(days: 1))), 
+            customHeight: buttonHeight, 
+            customWidth: buttonWidth, 
+            icon: SFIcon(SFIcons.sf_arrow_forward)
+          ),
+        )
+      ],
     );
   }
 }
