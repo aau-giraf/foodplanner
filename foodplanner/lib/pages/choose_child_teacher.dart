6c3593705385ce4dbfe77cbcdacf121f4de264a9
Dandan Zhao <dandanzhao08@gmail.com>
2025-11-10 08:54:31 +0100

    Main pages for parent and teacher done

---
+++ foodplanner/lib/pages/choose_child_teacher.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ChooseChildTeacher extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        toolbarHeight: 200,
+        centerTitle: true,
+        title: Padding(
+          padding: const EdgeInsets.only(top: 25),
+          child: Text(
+            'Vælg barn',
+            style: TextStyle(fontSize: 36),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
