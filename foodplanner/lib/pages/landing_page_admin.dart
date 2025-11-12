61553034315f7cdc1e3627eaa09b016b02b96b15
vish0018 <144022699+vish0018@users.noreply.github.com>
2025-11-12 14:58:56 +0100

    Mangler bare de lorte skygger
    Admin. Skole knap tilføjet. Skrift størrelser er rettet. Icon for skift profil er også rettet. Velkommen tingen siger kun fornavn nu.

--- foodplanner/lib/pages/landing_page_admin.dart
+++ foodplanner/lib/pages/landing_page_admin.dart
@@ -76,11 +76,21 @@ class _LandingPageAdminState extends State<AdminLandingPage> {
           }
         },
         {
+          'title': "Administrér Skole",
+          'cta': Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.school),
+              SizedBox(width: 10),
+            ],
+          ),
+        },
+        {
           'title': "Skift Rolle",
           'cta': Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Icon(Icons.switch_account),
+              Icon(Icons.group),
               SizedBox(width: 10),
             ],
           ),
@@ -124,33 +134,32 @@ class _LandingPageAdminState extends State<AdminLandingPage> {
         },
       ];
 
-  @override
-  Widget build(BuildContext context) {
+    @override
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text(
-            'Velkommen ${admin.firstName}${admin.lastName.isNotEmpty ? ' ${admin.lastName}' : ''}',
-            style: AppTextStyles.headline4,
-          ),
-        ),
         backgroundColor: Colors.white,
+        toolbarHeight: 225,
+        centerTitle: true,
+        title: Padding(
+          padding: const EdgeInsets.only(top: 70),
+          child: Text(
+            'Velkommen \n${admin.firstName ?? 'Admin'}',
+            style: TextStyle(fontSize: 36),
+            textAlign: TextAlign.center,
+          )
+        )
       ),
       backgroundColor: Colors.white,
+
       bottomNavigationBar: NavBar(),
       body: SingleChildScrollView(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-            minHeight: MediaQuery.of(context).size.height
-              - kToolbarHeight
-              - MediaQuery.of(context).padding.top,
-          ),
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const SizedBox(height: 40),
+                const SizedBox(height: 10),
                 ...adminActions.map((action) {
                   return Padding(
                     padding: const EdgeInsets.symmetric(vertical: 8),
@@ -176,9 +185,7 @@ class _LandingPageAdminState extends State<AdminLandingPage> {
                           children: [
                             Text(
                               action['title'] as String,
-                              style: AppTextStyles.buttonText.copyWith(
-                                color: Colors.black,
-                              ),
+                              style: TextStyle(fontSize: 20)
                             ),
                             Positioned(
                               right: 0,
@@ -194,7 +201,6 @@ class _LandingPageAdminState extends State<AdminLandingPage> {
             ),
           ),
         ),
-      ),
-    );
+      );
   }
 }
