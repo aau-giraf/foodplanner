59f330a3742a8565cd664aa9784bd4e14fea26eb
Camilla Bramsen <38404673+camillabramsen@users.noreply.github.com>
2025-11-18 13:23:41 +0100

    Update openapi_config.dart

--- foodplanner/lib/api/generator/openapi_config.dart
+++ foodplanner/lib/api/generator/openapi_config.dart
@@ -1,10 +1,8 @@
-// Openapi Generator last run: : 2025-11-04T13:20:24.499642
+// Openapi Generator last run: : 2024-11-25T14:25:31.343876
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 @Openapi(
-    additionalProperties:
-        AdditionalProperties(pubName: 'foodplanner_api', pubAuthor: 'Giraf'),
-    inputSpec:
+ @@ -8,4 +8,4 @@ import 'package:openapi_generator_annotations/openapi_generator_annotations.dart
         RemoteSpec(path: 'http://localhost:8080/swagger/v1/swagger.json'),
     generatorName: Generator.dart,
     outputDirectory: 'lib/api/openapi')
