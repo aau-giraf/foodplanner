a0990abefa9efd64bcb8f6685b5ac2eab2feb0aa
Camilla Bramsen <38404673+camillabramsen@users.noreply.github.com>
2025-11-19 15:17:56 +0100

    New unit-test

---
+++ foodplanner/test/page_tests/settings.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:go_router/go_router.dart';
+import 'package:foodplanner/services/user_service.dart';
+import 'package:foodplanner/pages/settings/settings.dart';
+import 'package:foodplanner/services/api_config.dart';
+
+void main(){
+  test('Fetch the user information', () async {
+    //final fetchUser = fetchUser();
+    final UserService service = UserService(apiUrl: ApiConfig.baseUrl);
+    final user = await service.fetchLoggedInUser();
+    expect(user.firstName, "C");
+  });
+}
