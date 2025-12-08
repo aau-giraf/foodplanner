31c86022fe5324525c7eb133f758b5f0c792c24a
SilleKorsbakke <151042508+SilleKorsbakke@users.noreply.github.com>
2025-12-08 13:42:36 +0100

    Update administrate_pupils.dart

--- foodplanner/lib/pages/settings/administrate_pupils.dart
+++ foodplanner/lib/pages/settings/administrate_pupils.dart
@@ -29,7 +29,7 @@ class AdministratePupilsState extends State<AdministratePupils>
   List<SchoolClass> schoolClasses = [];
   List<Pupil> filteredPupils = [];
   TextEditingController searchController = TextEditingController();
-  }
+  
 
   @override
   void initState() {
