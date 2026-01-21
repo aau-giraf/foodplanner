77235254a6b0e51bebf91aff21af65cbea3eaf46
SilleKorsbakke <151042508+SilleKorsbakke@users.noreply.github.com>
2026-01-21 14:25:09 +0100

    removing auto generated duplicate mock files

--- foodplanner/lib/navigation/navigation_service.dart
+++ foodplanner/lib/navigation/navigation_service.dart
@@ -1,9 +1,3 @@
-
-import 'package:foodplanner/api/openapi/lib/api.dart';
-import 'package:foodplanner/models/user_roles.dart';
-import 'package:foodplanner/navigation/navbar_strategy_mapper.dart';
-import 'package:foodplanner/navigation/navigation_strategy.dart';
-
 class NavigationService {
   static int currentPageIndex = 0;
 
